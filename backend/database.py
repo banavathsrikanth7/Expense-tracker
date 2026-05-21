25bca34b6db4f26ad951a5727b381d24a92e070d
banavathsrikanth7 <srikanth10.24@kgpian.iitkgp.ac.in>
2026-05-21 14:28:31 +0530

    update

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,21 @@
+import os
+
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:%40Sri939897@localhost/expense Tracker"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL"
+)
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+engine = create_engine(
+    DATABASE_URL
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
