8ca2b7cb4a91eda5c6f0348a57a044e2dab651d1
banavathsrikanth7 <srikanth10.24@kgpian.iitkgp.ac.in>
2026-06-04 06:56:57 +0530

    changed

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ import csv
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://expense-tracker-5qtqliqb7-banavath-srikanths-projects.vercel.app"],
+    allow_origins=["http://localhost:3000", "expense-tracker-4n1kucz2i-banavath-srikanths-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
