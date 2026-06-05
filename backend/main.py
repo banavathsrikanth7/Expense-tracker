aef73fc2809c1e2b7a729fe5b6c8692e57b42775
banavathsrikanth7 <srikanth10.24@kgpian.iitkgp.ac.in>
2026-06-05 08:17:33 +0530

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ import csv
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://expense-tracker-kappa-sepia-28.vercel.app"],
+    allow_origins=["http://localhost:3000","https://expense-tracker-kappa-sepia-28.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
